71af1e83ec5d3f8bc2de6a1af9fee23ff1c48510
NUB <nubcoders@gmail.com>
2025-05-09 13:18:29 +0000

    removed file logger

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
     handlers=[
-        logging.FileHandler('bot.log'),
         logging.StreamHandler()
     ]
 )
