013a7e01f54f52405059eecd58ca7776e949203f
reborndigitals <reborndigitals@gmail.com>
2025-07-19 08:06:20 +0530

    config.py

--- config.py
+++ config.py
@@ -10,15 +10,15 @@ from thumbnails import *
 from fonts import *
 
 # Use getenv for all sensitive/configurable values
-API_ID = os.getenv("API_ID",)
-API_HASH = os.getenv("API_HASH", "")
-STRING_SESSION = os.getenv("STRING_SESSION", "")
-GROUP = os.getenv("GROUP", "Hydrax_Music_update")
-CHANNEL = os.getenv("CHANNEL", "Hydrax_Music_update")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = os.getenv("OWNER_ID", 6076474757)
-LOGGER_ID = os.getenv("LOGGER_ID", None)
-mongodb = os.getenv("MONGODB_URI", "")
+API_ID = os.getenv("API_ID","10284859")
+API_HASH = os.getenv("API_HASH", "b0ad58eb8b845ba0003e0d9ce5fc2196")
+STRING_SESSION = os.getenv("STRING_SESSION", "BQDMBnkAhl63LfVwI1-Pu38Cj_U5J1fg94CoUuYa8VcOfu6JcKTsMf6IjdKxGuJUmTyLcycSMFw4H_amfXQ-6B8q0DoT1uA7xM4nAxR8XBQvDkepomQoB9qF6fiTRTBMJjIXFfVgNUY_5JI9PK08mJ26whDradE6lMEPlvVcR4Jto3KdaLpvTPE45nmx5VeAUl_LAgy4o3VgOa9S0yKy8Ha8d0ueGa_A8goiXm52ADheVq2EOcCuu09e-kJixlMujIuDrqLFd4S_RLlfTSvvmYAF0eOu1b6jz1vOwFahGBQCOT_eucH26KQe-VUlWlT-ECD3LyRJIwezaSrYZADjw_aXMUqQaQAAAAHjb-HaAA")
+GROUP = os.getenv("GROUP", "HeartBeat_Fam")
+CHANNEL = os.getenv("CHANNEL", "HeartBeat_Offi")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7405370458:AAHesjvcSy_ptb0XBaOliAiu1CUUCUvmVeE")
+OWNER_ID = os.getenv("OWNER_ID", "1281282633")
+LOGGER_ID = os.getenv("LOGGER_ID", "-1001735663878")
+mongodb = os.getenv("MONGODB_URI", "mongodb+srv://heartbeat:Beat7Heart@heartbeat.1h1nbxv.mongodb.net/?retryWrites=true&w=majority")
 # Working directory
 ggg = os.getcwd()
 
