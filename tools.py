e99452614003c557c61d43ee21b52b24384820bd
reborndigitals <reborndigitals@gmail.com>
2025-07-19 22:59:25 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -622,7 +622,7 @@ queue_styles = {
     5: """• ғᴜᴛᴜʀᴇ ᴛʀᴀᴄᴋ •
 ┏━━━━━━━━━━━
 ┣⟡ ᴍᴏᴅᴇ: {}
-┣⟡ ᴛɪᴛʟᴇ: {}[18]
+┣⟡ ᴛɪᴛʟᴇ: {}
 ┣⟡ ʟᴇɴɢᴛʜ: {}
 ┗⟡ ᴘᴏ𝗌ɪᴛɪᴏɴ: {}""",
 
@@ -709,7 +709,7 @@ play_styles = {
     5: """⋆｡°✩ ᴄᴜʀʀᴇɴᴛ ᴛʀᴀᴄᴋ ✩°｡⋆
 ┏━━━━━━━━━━━
 ┣⟡ ᴍᴏᴅᴇ: {}
-┣⟡ ᴛɪᴛʟᴇ: {}[18]
+┣⟡ ᴛɪᴛʟᴇ: {}
 ┣⟡ ʟᴇɴɢᴛʜ: {}
 ┗⟡ ᴅᴊ: {}""",
 
@@ -1145,7 +1145,7 @@ AudioQuality.STUDIO,
         ])
         sent_message = await clients["bot"].send_photo(
             message.chat.id, thumb, play_styles[int(gvarstatus(OWNER_ID, "format") or 5)].format(lightyagami(mode),
-f"[{lightyagami(title)}](https://t.me/{clients["bot"].me.username}?start=vidid_{extract_video_id(youtube_link)})" if not os.path.exists(youtube_link) else lightyagami(title), duration, by.mention()),
+f"[{lightyagami(title)[18]}](https://t.me/{clients["bot"].me.username}?start=vidid_{extract_video_id(youtube_link)})" if not os.path.exists(youtube_link) else lightyagami(title), duration, by.mention()),
             reply_markup=keyboard        )
         asyncio.create_task(autoleave_vc(sent_message, duration,chat))
         asyncio.create_task(update_progress_button(sent_message, duration,chat))
