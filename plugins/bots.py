e41653f6d13a801090c206a73cc625d8649832a4
reborndigitals <reborndigitals@gmail.com>
2025-07-22 19:10:17 +0530

    bots.py

--- plugins/bots.py
+++ plugins/bots.py
@@ -1328,7 +1328,7 @@ async def commands_handler(client, callback_query):
     # ---------- Routing ----------
     if data == "all":
         await callback_query.message.edit_caption(
-            caption="<blockquote>𓆩⃪〭𝆭〬፥܍𝐇єу 𝐌ιηє, 𝐓нιƨ 𝐈ƨ \n{botname}</blockquote>\n☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★\n<blockquote>🌹 𝐌ʏ 𝐕σιᴄɛ 𝐃ιƨƨσƖνɛƨ 𝐘σʋя Λᴄнɛƨ 𝐋ικɛ 𝐒ʋɢαя 𝐈и 𝐓ɛα</blockquote>\n☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★\n<blockquote>💕🦋𝐃σит тσʋᴄн мʏ [𝐂𝖗𝖚𝖘𝖍 🦇](https://t.me/Ghostt_Batt)</blockquote>",
+            caption="<blockquote>𓆩⃪〭𝆭〬፥܍𝐇єу 𝐌ιηє, 𝐓нιƨ 𝐈ƨ \n{botname}</blockquote>\n☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★\n<blockquote>🌹 𝐌ʏ 𝐕σιᴄɛ 𝐃ιƨƨσƖνɛƨ 𝐘σʋя Λᴄнɛƨ 𝐋ικɛ 𝐒ʋɢαя 𝐈и 𝐓ɛα</blockquote>\n☆ . * ● ¸ . ✦ .★° :. ★ * • ○ ° ★\n<blockquote>💕🦋𝐇ɛяɛ, 𝐒σмɛ 𝐓ιρƨ 𝐓σ 𝐈мρяɛƨƨ 𝐌ɛ</blockquote>",
             reply_markup=InlineKeyboardMarkup(category_buttons),
         )
     elif data == "playback":
